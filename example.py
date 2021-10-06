12b4029a374ef9801e7f427bd92783f4dea0be6b
Jegarde <jeskutti.n@gmail.com>
2021-10-06 20:17:40 +0300

    Example script for the module

---
+++ example.py
@@ -0,0 +1,24 @@
+# Import the module
+from recnetlogin import login_to_recnet
+
+if __name__ == "__main__":
+    import sys
+    import os
+
+    # Ask for login credentials
+    print("Log in to RecNet")
+    username = input("Username > ")
+    password = input("Password > ")
+
+    # Login
+    login = login_to_recnet(username, password)  # LOGIN FUNCTION FROM MODULE
+    if not login['success']:  # If the login was successful, this will be true
+        # Login failed.
+        sys.exit(login['error'])  # Print error included in the returned dictionary
+
+    # Login succeeded, export details
+    file_name = "token_details.txt"
+    with open(file_name, "w") as txt:
+        txt.write(f"Hello, {login['account_data']['displayName']}!\n\n{login['bearer_token']}\n\n{login['account_data']}")
+    os.startfile(file_name)  # Open the text file
+    sys.exit("Successful!")
