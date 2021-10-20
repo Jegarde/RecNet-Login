9ce3fb38cad972a5187ff6bcf67058b29768da28
Jesse <jeskutti.n@gmail.com>
2021-10-20 12:47:13 +0300

    Update example.py

--- example.py
+++ example.py
@@ -9,10 +9,10 @@ print("Success:", login.success)  # If login was successful
 print()
 print("Access token:", login.access_token)  # Access token / Bearer token
 print("Access token expiration:", login.access_expiration)  # Its expiration Unix timestamp
-print("Access token decoded:", login.decoded_access_token)  # Decoded
+print("Access token decoded:", login.access_decoded)  # Decoded
 print()
 print("Id token:", login.id_token)  # Id token
 print("Id token expiration:", login.id_expiration)  # Its expiration Unix timestamp
-print("Id token decoded:", login.decoded_id_token)  # Decoded
+print("Id token decoded:", login.id_decoded)  # Decoded
 print()
 print("Data:", login.data)  # Account data from /account/me
