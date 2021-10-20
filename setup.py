120d8358553ac3673072f4b9f30af646330632a3
Jesse <jeskutti.n@gmail.com>
2021-10-20 12:17:42 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ setup(
     name='recnetlogin',
     author='Jegarde',
     url='https://github.com/Jegarde/RecNet-Login/',
-    version=0.2,
+    version=0.3,
     packages=['recnetlogin'],
     license='MIT',
-    description='A package that allows you to acquire a bearer token from RecNet with account credentials.',
-    install_requires=['requests==2.25.1'],
+    description='A package that allows you to acquire a bearer token and more from RecNet with account credentials.',
+    install_requires=['requests==2.25.1', 'PyJWT==2.3.0'],
     python_requires='>=3.9.0'
 )
