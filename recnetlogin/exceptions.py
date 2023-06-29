ad92e3699aadf2040b96f8f5127b44103974fdf7
Jegarde <jesse.tapani.nieminen@gmail.com>
2023-06-29 20:37:59 +0300

    Rename exceptions

--- recnetlogin/exceptions.py
+++ recnetlogin/exceptions.py
@@ -1,14 +1,14 @@
 class CookieMissing(Exception):
     """Raised when RecNet cookie is missing from environmental variables"""
     def __init__(self) -> None:
-        super().__init__("RecNetLogin - Missing RN_COOKIE environmental variable! Try restarting your computer.")
+        super().__init__("Missing RN_COOKIE environmental variable! Try restarting your computer or make a .env.secret file.")
 
 class InvalidLocalCookie(Exception):
     """Raised when RecNet cookie is not valid for getting the token from a .env.secret file"""
     def __init__(self) -> None:
-        super().__init__("RecNetLogin - RN_COOKIE is not valid or has expired! Check your .env.secret file and its path.")
+        super().__init__("RN_COOKIE is not valid or has expired! Check your .env.secret file and its path.")
 
 class InvalidSystemCookie(Exception):
     """Raised when RecNet cookie is not valid for getting the token from system environment variables"""
     def __init__(self) -> None:
-        super().__init__("RecNetLogin - RN_COOKIE environmental variable is not valid or has expired! Check your environment variables and try restarting your computer.")
+        super().__init__("RN_COOKIE environmental variable is not valid or has expired! Check your environment variables and try restarting your computer.")
